05823c07a09392a1f7423ddf335fffd98c8b9478
nominori-dev <antonshimchico37@gmail.com>
2024-01-27 18:53:34 +0100

    feat: project initialization

---
+++ src/test/java/com/nominori/librarycrud/LibraryCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nominori.librarycrud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryCrudApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
